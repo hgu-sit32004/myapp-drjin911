b971dd8e663b6ed86c32546c3996a6345700f692
drjin911 <21600745@handong.edu>
2018-06-05 14:36:54 +0900

    todo 1

---
+++ Todo/ToDoItem.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoItem.swift
+//  Todo
+//
+//  Copyright © 2016 YiGu. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem: NSObject {
+  var id: String
+  var image: String
+  var title: String
+  var date: Date
+  var descriptional: String
+  
+    init(id: String, image: String, title: String, date: Date, description: String) {
+    self.id = id
+    self.image = image
+    self.title = title
+    self.date = date
+    self.descriptional = description
+  }
+}
