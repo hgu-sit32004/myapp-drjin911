e9db4fc837f61be4bf127fa96dd85c743d72fffb
drjin911 <21600745@handong.edu>
2018-06-07 22:16:40 +0900

    other app

--- Todo/DetailViewController.swift
+++ Todo/DetailViewController.swift
@@ -1,110 +1,31 @@
 //
-//  DetailViewController.swift
-//  Todo
+//  NoteTableViewCell.swift
+//  ElevenNote
 //
+//  Copyright (c) 2014 ElevenFifty. All rights reserved.
 //
 
 import UIKit
 
-class DetailViewController: UIViewController {
-  
-  @IBOutlet weak var childButton: UIButton!
-  @IBOutlet weak var phoneButton: UIButton!
-  @IBOutlet weak var shoppingCartButton: UIButton!
-  @IBOutlet weak var travelButton: UIButton!
-  @IBOutlet weak var todoTitleLabel: UITextField!
-  @IBOutlet weak var todoDatePicker: UIDatePicker!
-  @IBOutlet weak var todoDescription: UITextField!
-  
+class NoteDetailTableViewCell : UITableViewCell {
     
-  var todo: ToDoItem?
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    /*
-    if let todo = todo {
-      self.title = "Edit Todo"
-      if todo.image == "child-selected"{
-        childButton.isSelected = true
-      }
-      else if todo.image == "phone-selected"{
-        phoneButton.isSelected = true
-      }
-      else if todo.image == "shopping-cart-selected"{
-        shoppingCartButton.isSelected = true
-      }
-      else if todo.image == "travel-selected"{
-        travelButton.isSelected = true
-      }
-      
-      todoTitleLabel.text = todo.title
-      todoDatePicker.setDate(todo.date, animated: false)
-      todoDescription.text = todo.descriptional
-    } else {
-      title = "New Todo"
-      childButton.isSelected = true
-    }*/
-  }
-  
-  // MARK: type select
-  @IBAction func selectChild(_ sender: AnyObject) {
-    resetButtons()
-    childButton.isSelected = true
-  }
-  
-  @IBAction func selectPhone(_ sender: AnyObject) {
-    resetButtons()
-    phoneButton.isSelected = true
-  }
-  
-  @IBAction func selectShoppingCart(_ sender: AnyObject) {
-    resetButtons()
-    shoppingCartButton.isSelected = true
-  }
-  
-  @IBAction func selectTravel(_ sender: AnyObject) {
-    resetButtons()
-    travelButton.isSelected = true
-  }
-  
-  func resetButtons() {
-    childButton.isSelected = false
-    phoneButton.isSelected = false
-    shoppingCartButton.isSelected = false
-    travelButton.isSelected = false
-  }
-  
-  // MARK: create or edit a new todo
-  @IBAction func tapDone(_ sender: AnyObject) {
-    var image = ""
-    if childButton.isSelected {
-      image = "child-selected"
-    }
-    else if phoneButton.isSelected {
-      image = "phone-selected"
-    }
-    else if shoppingCartButton.isSelected {
-      image = "shopping-cart-selected"
-    }
-    else if travelButton.isSelected {
-      image = "travel-selected"
+    // The note currently being shown
+    weak var theNote : Note!
+    
+    // Interface builder outlets
+    @IBOutlet weak var noteTitleLabel : UILabel!
+    @IBOutlet weak var noteDateLabel : UILabel!
+    @IBOutlet weak var noteTextLabel : UILabel!
+    
+    // Insert note contents into the cell
+    func setupCell(theNote:Note) {
+        // Save a weak reference to the note
+        self.theNote = theNote
+        
+        // Update the cell
+        noteTitleLabel.text = theNote.title
+        noteTextLabel.text = theNote.text
+        noteDateLabel.text = theNote.shortDate
     }
     
-    if let todo = todo {
-      todo.image = image
-      todo.title = todoTitleLabel.text!
-      todo.date = todoDatePicker.date
-    } else {
-      let uuid = UUID().uuidString
-        todo = ToDoItem(id: uuid, image: image, title: todoTitleLabel.text!, date: todoDatePicker.date, description: todoDescription.text!)
-        todos.append(todo!)
-    }
-    
-    let _ = navigationController?.popToRootViewController(animated: true)
-  }
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    super.touchesBegan(touches, with: event)
-    view.endEditing(true)
-  }
 }
