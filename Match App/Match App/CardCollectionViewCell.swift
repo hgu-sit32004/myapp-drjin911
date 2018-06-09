3d866f3b95b6a8ca6e41108b7a9725f213cfa3f6
drjin911 <21600745@handong.edu>
2018-06-09 14:48:33 +0900

    lecture 5

--- Match App/Match App/CardCollectionViewCell.swift
+++ Match App/Match App/CardCollectionViewCell.swift
@@ -11,5 +11,7 @@ import UIKit
 class CardCollectionViewCell: UICollectionViewCell {
     
     
+    @IBOutlet weak var frontImageView: UIImageView!
     
+    @IBOutlet weak var backImageView: UIImageView!
 }
--- Match App/Match App/ViewController.swift
+++ Match App/Match App/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var model = CardModel()
     var cardArray = [Card]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         //Call the getCards method of the card model
         cardArray = model.getCards()
         
@@ -26,7 +31,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 
 }
 
